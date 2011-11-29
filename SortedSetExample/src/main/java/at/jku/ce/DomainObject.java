225bb88324937557da6d82dab4e15f3f094da93a
Arthur <20042005@atn.nu>
2011-11-29 14:27:16 +0100

    <TAG2> Arthur

--- SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
@@ -1,7 +1,8 @@
 package at.jku.ce;
 
 public class DomainObject {
-public String id;public DomainObject(String id) { super(); this.id = id;}
+public String id;
+private String name;public DomainObject(String id) { super(); this.id = id;}
 	public DomainObject() {
 		super();
 	}
