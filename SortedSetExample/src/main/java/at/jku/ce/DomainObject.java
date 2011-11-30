2230ee10b93e881d80e79ced06fcbc52a53ea5e6
Barbara <Irtschi@hotmail.com>
2011-11-30 14:25:24 +0100

    <Tag 7> Barbara: hashCode

--- SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
@@ -24,4 +24,11 @@ public String getComment() { return comment;}
 	public void setName(final String pname) { this.name = pname;}
 	public String toString() { return "DomainObject [name=" + name + ", comment=" + comment + ", uuid=" + uuid + "]"; }
 	public boolean equals(Object obj) { if (!(obj instanceof DomainObject)) { return false; } DomainObject domainObj = (DomainObject) obj; return uuid.equals(domainObj.getUuid()); }
+	public int hashCode() {
+if (uuid != null) {
+return uuid.hashCode();
+} else {
+return super.hashCode();
+}
+}
 
